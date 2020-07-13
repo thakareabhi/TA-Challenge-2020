4cd0c0f77010523f8cd3c379ea50434907186f6e
ABHINAV THAKARE <51523495+thakareabhi@users.noreply.github.com>
2020-07-13 22:13:44 +0530

    Update Question5.java

--- java/src/main/java/skeleton/answers/Question5.java
+++ java/src/main/java/skeleton/answers/Question5.java
@@ -4,6 +4,57 @@ public class Question5 {
 
     public static int shareExchange(int[] allowedAllocations, int totalValue) {
         // TODO Auto-generated method stub
-        return -5;
+        
+        Arrays.sort(allowedAllocations);
+        
+        int flag=0;
+        
+        for (int index = 0; index < allowedAllocations.length; index++) 
+        {
+            if(allowedAllocations[index] == totalValue)
+            {
+                   flag=1;
+                   break;
+            }
+            if(allowedAllocations[index]<totalValue)
+            {
+                break;
+            }
+        }
+        
+        
+        if(flag==0)
+        {
+                for (int index = 0; index < allowedAllocations.length; index++) 
+                {
+                    if(arr[index] == (totalValue/2))
+                    {
+                           flag=2;
+                           break;
+                    }
+                    if(arr[index]<totalValue)
+                    {
+                        break;
+                    }
+                }
+        }
+        
+        int pcnt=0;
+        if(flag==0)
+        {
+                for(int i=0;i<allowedAllocations.length;i++)
+                {
+                    
+                }
+        }
+        
+        if(flag!=0)
+        {
+            return flag;
+        }
+         else
+        {
+             return 0;
+        }
     }
 }
