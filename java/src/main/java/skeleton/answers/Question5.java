6d180cb339d2caf26b187bc5b41ed3cd4eecd4bb
ABHINAV THAKARE <51523495+thakareabhi@users.noreply.github.com>
2020-07-14 11:43:01 +0530

    Update Question5.java

--- java/src/main/java/skeleton/answers/Question5.java
+++ java/src/main/java/skeleton/answers/Question5.java
@@ -2,22 +2,10 @@ package skeleton.answers;
 
 public class Question5 {
 
-    public static int shareExchange(int[] numbers, int sum) {
-     
-                ArrayList minSet = new ArrayList();
-                 int flag=0;
-                for(int i=numbers.length-1; i&gt;=0; i--) 
-                {
-                           sum = sum - numbers[i];
-                           minSet.add( new Integer(numbers[i]) );
-                                  if(sum &lt;=0) 
-                                  {
-                                     return minSet.size();
-                                  }
-                }
+    public static int shareExchange(int[] numbers, int sum){
           
                     
-           return 0;
+           return -5;
         
     }
 }
