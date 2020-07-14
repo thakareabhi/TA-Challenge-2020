44f1a9943a3d6167b40516940ee65b2f395122c0
ABHINAV THAKARE <51523495+thakareabhi@users.noreply.github.com>
2020-07-14 12:54:52 +0530

    Update Question5.py

--- python/Question5.py
+++ python/Question5.py
@@ -1,25 +1,23 @@
-import sys
+
 # modify this function, and create other functions below as you wish
 def question05(num_list, value):
     ist_len = len(num_list)
 
-   
 
-       
+   
     value_table = [0]
 
    
     for i in range(1, value+1):
-        value_table.append(sys.maxsize);
+        value_table.append(9223372036854775807);
 
-    
+   
     for i in range(1, value+1):
         for j in range(0, list_len):
             if (num_list[j] <= i):
                   tmp = value_table[i-num_list[j]]
-                  if ((tmp != sys.maxsize) and (tmp + 1 < value_table[i])):
+                  if ((tmp !=9223372036854775807) and (tmp + 1 < value_table[i])):
                       value_table[i] = tmp + 1
 
-     
     return value_table[value]
     
